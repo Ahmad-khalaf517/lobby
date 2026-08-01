06031005b446550a08883aaf0fd175229c2d22db
Mohammad Hajeer <mohammadhajeer002@gmail.com>
2026-08-01 23:27:09 +0300

    feat: implement not found and unauthorized pages with animations and improved layout

--- apps/web/src/app/features/errors/not-found-page/not-found-page.ts
+++ apps/web/src/app/features/errors/not-found-page/not-found-page.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LogoComponent } from '../../../shared/ui/logo/lobby-logo.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
-  imports: [],
+  imports: [RouterLink, LogoComponent],
   templateUrl: './not-found-page.html',
   styleUrl: './not-found-page.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotFoundPage {}
--- apps/web/src/app/features/errors/unauthorized-page/unauthorized-page.ts
+++ apps/web/src/app/features/errors/unauthorized-page/unauthorized-page.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LogoComponent } from '../../../shared/ui/logo/lobby-logo.component';
 
 @Component({
   selector: 'app-unauthorized-page',
-  imports: [],
+  imports: [RouterLink, LogoComponent],
   templateUrl: './unauthorized-page.html',
   styleUrl: './unauthorized-page.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UnauthorizedPage {}
