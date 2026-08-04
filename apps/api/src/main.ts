bf3e152c3cd881f0b062c7d578cc8eb50a683b63
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2026-08-04 06:00:27 +0300

    Change domain url of web app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   const allowedOrigins =
-    'http://localhost:4200,http://127.0.0.1:4200,http://127.0.0.1:5500,http://localhost:5500,https://lobby-api.vercel.app'
+    'http://localhost:4200,http://127.0.0.1:4200,http://127.0.0.1:5500,http://localhost:5500,https://lobby-hub.vercel.app'
       .split(',')
       .map((origin) => origin.trim())
       .filter(Boolean);
