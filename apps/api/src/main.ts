62f3a65e9a1ec9bbe85c8306cf58a2da2e48caf1
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2026-08-04 04:22:39 +0300

    Add vercel url (app domain url) to allowed origins in cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,13 +8,11 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = (
-    process.env.CORS_ORIGINS ??
-    'http://localhost:4200,http://127.0.0.1:4200,http://127.0.0.1:5500,http://localhost:5500'
-  )
-    .split(',')
-    .map((origin) => origin.trim())
-    .filter(Boolean);
+  const allowedOrigins =
+    'http://localhost:4200,http://127.0.0.1:4200,http://127.0.0.1:5500,http://localhost:5500,https://lobby-api.vercel.app'
+      .split(',')
+      .map((origin) => origin.trim())
+      .filter(Boolean);
 
   app.enableCors({
     origin: allowedOrigins,
